473268c01d164dac482dfa414660d230cc52c562
yoodk777 <yu2136@naver.com>
2017-06-29 11:18:28 +0900

    react -1
    리액트 프로젝트 1강좌

---
+++ server/main.js
@@ -0,0 +1,21 @@
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+
+const devPort = 4000;
+
+/*
+    Express Codes
+*/
+
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
